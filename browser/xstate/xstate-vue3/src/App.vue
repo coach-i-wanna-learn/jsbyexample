9f93922a15056c829e56628c3d2faa541a986139
蒋波 <jiangbo0216@gmail.com>
2023-04-19 22:09:48 +0800

    xstate vue3

--- browser/xstate/xstate-vue3/src/App.vue
+++ browser/xstate/xstate-vue3/src/App.vue
@@ -1,90 +1,3 @@
-// <script setup lang="ts">
-// import { RouterLink, RouterView } from 'vue-router'
-// import HelloWorld from './components/HelloWorld.vue'
-// </script>
-
-// <template>
-//   <header>
-//     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-//     <div class="wrapper">
-//       <HelloWorld msg="You did it!" />
-
-//       <nav>
-//         <RouterLink to="/">Home</RouterLink>
-//         <RouterLink to="/about">About</RouterLink>
-//       </nav>
-//     </div>
-//   </header>
-
-//   <RouterView />
-// </template>
-
-// <style scoped>
-// header {
-//   line-height: 1.5;
-//   max-height: 100vh;
-// }
-
-// .logo {
-//   display: block;
-//   margin: 0 auto 2rem;
-// }
-
-// nav {
-//   width: 100%;
-//   font-size: 12px;
-//   text-align: center;
-//   margin-top: 2rem;
-// }
-
-// nav a.router-link-exact-active {
-//   color: var(--color-text);
-// }
-
-// nav a.router-link-exact-active:hover {
-//   background-color: transparent;
-// }
-
-// nav a {
-//   display: inline-block;
-//   padding: 0 1rem;
-//   border-left: 1px solid var(--color-border);
-// }
-
-// nav a:first-of-type {
-//   border: 0;
-// }
-
-// @media (min-width: 1024px) {
-//   header {
-//     display: flex;
-//     place-items: center;
-//     padding-right: calc(var(--section-gap) / 2);
-//   }
-
-//   .logo {
-//     margin: 0 2rem 0 0;
-//   }
-
-//   header .wrapper {
-//     display: flex;
-//     place-items: flex-start;
-//     flex-wrap: wrap;
-//   }
-
-//   nav {
-//     text-align: left;
-//     margin-left: -1rem;
-//     font-size: 1rem;
-
-//     padding: 1rem 0;
-//     margin-top: 1rem;
-//   }
-// }
-// </style>
-
-
 <template>
   <div id="app">
     <h1>XState Vue 3 Template</h1>
@@ -101,11 +14,11 @@
 
 <script>
 import { createMachine, assign } from "xstate";
-import { useMachine } from "@xstate/vue";
+import { useMachine } from '@xstate/vue';
 
 const toggleMachine = createMachine({
   id: "toggle",
-  initial: "inactive",
+  initial: 'inactive',
   context: {
     count: 0,
   },
@@ -115,21 +28,22 @@ const toggleMachine = createMachine({
     },
     active: {
       entry: assign({ count: (ctx) => ctx.count + 1 }),
-      on: { TOGGLE: "inactive" },
+      on: { TOGGLE: 'inactive' },
     },
   },
   predictableActionArguments: true,
-});
+})
 
 export default {
-  setup() {
+  setup () {
     const { state, send } = useMachine(toggleMachine);
+    console.log(state);
     return {
       state,
-      send,
-    };
-  },
-};
+      send
+    }
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
