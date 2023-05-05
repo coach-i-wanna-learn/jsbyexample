6a518b948aa134724acd1601370fdb0b88b035b9
蒋波 <jiangbo0216@gmail.com>
2023-05-05 09:26:12 +0800

    vue

---
+++ browser/vue/nuxt-playground/pages/form-bind.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import {ref} from 'vue'
+
+  const text = ref('')
+
+</script>
+
+<template>
+<input type="text" v-model="text" placeholder="type here">
+<p>{{text}}</p>
+</template>
+
+<style>
+
+</style>
---
+++ browser/vue/nuxt-playground/pages/state.vue
@@ -0,0 +1,14 @@
+<script setup> 
+import { reactive, ref} from 'vue'
+
+const couter = reactive({count: 0}) // Proxy
+const message = ref('hello world') // ref 能够克服 reactive 的缺点，保证了 类似 结构赋值的场景仍然能够保持响应式
+
+// https://cn.vuejs.org/guide/essentials/reactivity-fundamentals.html#limitations-of-reactive
+</script>
+
+<template>
+  <h1>{{message}}</h1>
+  <!-- 任何有效的 js 表达式 -->
+  <p>Count is: {{couter.count}}</p> 
+</template>
